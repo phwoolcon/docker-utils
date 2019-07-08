20ff5becd1b9b4787365e8ec0c9c8b9468819655
Christopher CHEN <fishdrowned@gmail.com>
2019-07-08 20:44:05 +0800

    feat: ability to specify repo for `pick-mirror.sh`

--- alpine/pick-mirror.sh
+++ alpine/pick-mirror.sh
@@ -1,33 +1,53 @@
 #!/usr/bin/env sh
 
 VERSION=${1:-latest-stable}
-echo Detecting fastest mirror for alpine ${VERSION} in 2 seconds...
-> ping.log
-> ping.error.log
+
+main() {
+    if [ -n "${ALPINE_REPO}" ]; then
+        save_repo ${ALPINE_REPO} ${VERSION}
+    else
+        run_detect
+    fi
+}
+
+run_detect() {
+    echo Detecting fastest mirror for alpine ${VERSION} in 2 seconds...
+    > ping.log
+    > ping.error.log
+    wget -O- http://dl-cdn.alpinelinux.org/alpine/MIRRORS.txt 2>/dev/null | while read URL; do
+        { detect ${URL} >> ping.log; } &
+        { sleep .3; detect ${URL} >> ping.log; } &
+        { sleep .6; detect ${URL} >> ping.log; } &
+    done;
+    sleep 2;
+    REPO=$(awk '{SUM[$2] += $1; COUNT[$2]++};
+    END {
+        for (k in SUM) {
+            print SUM[k] / COUNT[k], k, COUNT[k]
+        }
+    }' ping.log | sort -n | head -n1 | awk '{print $2}');
+    save_repo ${REPO} ${VERSION}
+}
+
 request() {
     wget $1 -O /dev/null 2>>ping.error.log;
 }
+
 timestamp() {
     awk '{print $1*1000}' /proc/uptime;
 }
+
 detect() {
     START=$(timestamp)
     request $1 || exit
     END=$(timestamp)
     echo $(($END-$START)) $1 ${START} ${END};
 }
-wget -O- http://dl-cdn.alpinelinux.org/alpine/MIRRORS.txt 2>/dev/null | while read URL; do
-    { detect ${URL} >> ping.log; } &
-    { sleep .3; detect ${URL} >> ping.log; } &
-    { sleep .6; detect ${URL} >> ping.log; } &
-done;
-sleep 2;
-REPO=$(awk '{SUM[$2] += $1; COUNT[$2]++};
-END {
-    for (k in SUM) {
-        print SUM[k] / COUNT[k], k, COUNT[k]
-    }
-}' ping.log | sort -n | head -n1 | awk '{print $2}');
-echo ${REPO}
-printf "${REPO}${VERSION}/main
-${REPO}${VERSION}/community\n" > /etc/apk/repositories
+
+save_repo() {
+    echo $1
+    printf "$1$2/main
+$1$2/community\n" > /etc/apk/repositories
+}
+
+main
