23f98300c33ff999d475b20c654fc530e9c342e5
Christopher CHEN <fishdrowned@gmail.com>
2019-09-06 14:06:18 +0800

    fix: remove irrelevant files

--- docker-host.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-docker network create -d macvlan --subnet=192.168.1.0/24 --gateway=192.168.1.1 -o parent=enp1s0 vlan
--- iptables.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-ipset -N redir iphash
-iptables -t nat -A PREROUTING -p tcp -m set --match-set redir dst -j REDIRECT --to-port 1080
