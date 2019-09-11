f57efd8c82b6f33948d197b7718124c9c4ce8c3f
Christopher CHEN <fishdrowned@gmail.com>
2019-09-11 17:57:20 +0800

    fix: add sudo for installation

--- README.md
+++ README.md
@@ -2,6 +2,8 @@ Secure LAN Storage
 ==
 ## Usage
 ```bash
+git clone --single-branch --branch=lan-storage https://github.com/phwoolcon/docker-utils.git ~/lan-store
+cd ~/lan-store
 ./run.sh
 ```
 
--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@ function main() {
 
 function detectAndInstallDocker() {
     which docker-compose > /dev/null && return
-    apt-get update
-    apt-get remove -y docker docker-engine docker.io containerd runc
-    apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+    sudo apt-get update
+    sudo apt-get remove -y docker docker-engine docker.io containerd runc
+    sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
     echo "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
         | sudo tee /etc/apt/sources.list.d/docker.list
-    apt-get update
-    apt-get install -y containerd.io docker-ce-cli docker-ce docker-compose
+    sudo apt-get update
+    sudo apt-get install -y containerd.io docker-ce-cli docker-ce docker-compose
 }
 
 function askEnvConfig() {
@@ -36,17 +36,17 @@ function askForEnv() {
     echo $1=${READ_VAL} >> ./.env
 }
 
-function runServices() {
-    docker-compose up -d --build && docker image prune -f
-    docker-compose ps
-}
-
 function changeBareMetalSshPort() {
     grep -P '^Port\s+22$' /etc/ssh/sshd_config > /dev/null && sudo sed -Ei 's|^Port\s+22$|#Port 22|g'
     grep -P '^Port\s\d+\s+# sshfs$' /etc/ssh/sshd_config > /dev/null && return
     read -p "Change the SSH port of the bare metal server: " SSH_PORT
     echo "Port ${SSH_PORT} # sshfs" | sudo tee -a /etc/ssh/sshd_config > /dev/null
     sudo service ssh restart
+}
+
+function runServices() {
+    docker-compose up -d --build && docker image prune -f
+    docker-compose ps
 }
 
 main
