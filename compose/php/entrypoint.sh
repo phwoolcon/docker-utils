68076af97716456be4cbd38cf775cd701f3b4277
Christopher <fishdrowned@gmail.com>
2021-06-22 14:37:40 +0800

    fix(php): typo

--- compose/php/entrypoint.sh
+++ compose/php/entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-[ -f /.initialied ] || {
+[ -f /.initialized ] || {
   wget https://ghu.fishdrowned.workers.dev/phwoolcon/docker-utils/master/alpine/pick-mirror -O /pick-mirror;
   chmod +x /pick-mirror;
   /pick-mirror;
   apk update;
-  apk add imagemagick-dev libpng-dev libzip-dev $PHPIZE_DEPS;
+  apk add imagemagick-dev libpng-dev libzip-dev git openssh-client $PHPIZE_DEPS;
   echo | pecl install imagick;
   echo | pecl install redis;
   docker-php-ext-enable imagick redis;
   docker-php-ext-install exif gd mysqli opcache pdo_mysql zip;
-  touch /.initialied;
+  touch /.initialized;
 };
 php-fpm;
